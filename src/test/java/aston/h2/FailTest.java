5e6f4564274296a3141901dd65d215b33a34beec
msyaskov <ms.yaskov@gmail.com>
2024-02-18 16:59:34 +0300

    add FailTest

---
+++ src/test/java/aston/h2/FailTest.java
@@ -0,0 +1,14 @@
+package aston.h2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FailTest {
+
+    @Test
+    public void failTest() {
+        assertEquals(1,2);
+    }
+
+}
